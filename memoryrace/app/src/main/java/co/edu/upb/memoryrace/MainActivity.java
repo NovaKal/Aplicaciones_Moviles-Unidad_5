9c2c19f7f243c8d061432b57d4850c7a965d6dc2
NovaKal <kevin.alejandro.vasco@gmail.com>
2023-05-18 22:26:50 -0500

    Started MainActivity and Refactor xmls

--- memoryrace/app/src/main/java/co/edu/upb/memoryrace/MainActivity.java
+++ memoryrace/app/src/main/java/co/edu/upb/memoryrace/MainActivity.java
@@ -1,14 +1,29 @@
 package co.edu.upb.memoryrace;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        Button button_login = findViewById(R.id.login_button_login);
+        ImageButton button_exit = findViewById(R.id.login_imageButton_exit);
+
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_home);
+            }
+        });
+
+        button_exit.setOnClickListener(v -> finish());
     }
 }
