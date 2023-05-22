f8990830e8bb5ae9fcb49359846d74c88eba799b
NovaKal <kevin.alejandro.vasco@gmail.com>
2023-05-21 23:10:21 -0500

    Added About Us Screen and Functionailty

--- memoryrace/app/src/main/java/co/edu/upb/memoryrace/About.java
+++ memoryrace/app/src/main/java/co/edu/upb/memoryrace/About.java
@@ -1,13 +1,29 @@
 package co.edu.upb.memoryrace;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class About extends AppCompatActivity {
+
+    ImageButton about_imageButton_exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        about_imageButton_exit = findViewById(R.id.about_imageButton_exit);
+
+        about_imageButton_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Home.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
